3fe6bc763c0dcb58f00147908f2b3efaeb8734c5
Timothy Chew <timothyrchew@gmail.com>
2018-04-05 12:02:08 -0500

    feat(exposure): set spot exposure on faces

--- ios/RN/RNCamera.h
+++ ios/RN/RNCamera.h
@@ -17,7 +17,6 @@
 @property(nonatomic, strong) AVCaptureSession *session;
 @property(nonatomic, strong) AVCaptureDeviceInput *videoCaptureDeviceInput;
 @property(nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
-//@property(nonatomic, strong) AVCaptureMovieFileOutput *movieFileOutput;
 @property(nonatomic, strong) AVCaptureMetadataOutput *metadataOutput;
 @property(nonatomic, strong) id runtimeErrorHandlingObserver;
 @property(nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
@@ -46,6 +45,8 @@
 @property(nonatomic, assign) BOOL canAppendBuffer;
 @property(nonatomic, assign) CMTime bufferTimestamp;
 @property(nonatomic, assign) Float64 maxDuration;
+@property(nonatomic, assign) BOOL exposureTimeout;
+@property (nonatomic, strong) NSTimer *exposureTimer;
 
 - (id)initWithBridge:(RCTBridge *)bridge;
 - (void)updateType;
